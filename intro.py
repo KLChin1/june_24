8f3e47f79df8d98f014b7cbacf90980c443cdb2e
Spencer Rauch <rauchs@gmail.com>
2024-05-23 09:46:08 -0700

    python primer

---
+++ intro.py
@@ -0,0 +1,19 @@
+print("Hello World with a change")
+
+#comments! Comments use the hashtag or pound 
+"""
+For all 
+intents and purposes
+this is a multiline comment
+"""
+
+my_value = 9
+#snake_case! snake_case_looks_like_this
+#exceptions: Classes and Global vars
+
+if my_value == 8:
+    print("That is an 8")
+elif my_value == 7:
+    print("This is a seven")
+else:
+    print("Oh it wasn't an 8 or a 7 afterall")
