7b20abc5b3b925f67b56a7f028c8e942106e4e83
Spencer Rauch <rauchs@gmail.com>
2024-06-07 09:08:57 -0700

    solution

--- algos/W1D5.js
+++ algos/W1D5.js
@@ -35,10 +35,47 @@ const expected5 = "ab17";
  * @returns {string} The given string encoded.
  */
 // RIOT  Read Input Output Talk
+// Pseudocode:
+//   Create new string to return
+//   Iterate through the original string
+//   -- consider current character, look ahead to see how many duplicates exist
+//   -- if more than one, add character and duplicate count to new string
+//      -- else just add character
+//   -- continue iterating from last instance of the character we're considering
+//   Compare encoded string to new string, return encoded string only if it is shorter, otherwise return 
+
 
 function encodeStr(str) {
-    //Your code here
+    let encoded = ""; //string to be returned
+
+    // ? Process the original string
+    for (let i = 0; i < str.length; i++) { //iterate through original string
+        let currChar = str[i]; //consider the current character
+        let dupeCount = 1; // we have seen it once so far
+
+        // ? This loop moves i forward and counts duplicates for the current char
+        while (str[i + 1] === currChar) { //while the next character is equal to the current char
+            dupeCount++; //increase amount of dupes,
+            i++; //increment i to check next character
+        }
+
+        // ? Adding encoded information to the string
+        if (dupeCount === 1) { //if we only found one character
+            encoded += currChar //just add that character in
+        }
+        // else if (dupeCount === 2){ // ! we could add an additional check for a count of two
+        //     encoded += currChar + currChar
+        // }
+        else {
+            encoded += currChar + dupeCount; //otherwise add the character and the count
+        }
+    }
+
+    //? Return the encoded string only if it is shorter, otherwise return original string
+    return encoded.length < str.length ? encoded : str;
+    //ternary: expression ? value if true : value if false
 }
+
 
 console.log(encodeStr(str1)); // Expected: "a4b2cd3a3" or "a4bbcd3a3";
 console.log(encodeStr(str2)); // Expected: ""
